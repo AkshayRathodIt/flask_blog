d20187050f4d863b9eca949692dc8e69074f22cf
AkshayRathodIt <akshayrathodit@gmail.com>
2023-07-16 11:05:57 +0530

    added login logout funcationality in our website and also protected routes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app) 
+login_mannager = LoginManager(app)
+login_mannager.login_view = 'login'
+login_mannager.login_message_category = 'info'
 
 from flaskblog import routs
