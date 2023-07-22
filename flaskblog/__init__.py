b1a2264a21cdf85942256e4be839fb6c3dadbf7f
AkshayRathodIt <akshayrathodit@gmail.com>
2023-07-22 11:32:59 +0530

    added environment and password reset functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
 
 db = SQLAlchemy(app)
 
@@ -13,5 +23,6 @@ bcrypt = Bcrypt(app)
 login_mannager = LoginManager(app)
 login_mannager.login_view = 'login'
 login_mannager.login_message_category = 'info'
+mail = Mail(app)
 
 from flaskblog import routs
